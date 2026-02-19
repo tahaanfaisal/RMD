a5d90e8ab26a2865f01aa3634947bd19060164de
tahaanfaisal <tahaanfaisal@gmail.com>
2026-02-19 11:03:38 +0000

    done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,15 @@
 const name = "Faisal"
 const repoCount = 50
 
-console.log(name + repoCount + " Value");
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Faisalft')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
